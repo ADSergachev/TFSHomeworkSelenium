cae08cdc26e3c6c41c00ba67a9a76b439be36bfe
ADSergachev <sergachevad@gmail.com>
2018-04-08 19:43:04 +0300

    Final commit

--- src/test/java/PaymentTest.java
+++ src/test/java/PaymentTest.java
@@ -6,15 +6,19 @@ import org.openqa.selenium.*;
 
 public class PaymentTest extends BaseRunner {
 
+  //пока не совсем понял ожидания, поэтому просто установил Thread.sleep();
   @Test
-  public void testPayment(){
+  public void testPayment() throws InterruptedException {
     driver.get(BASE_URL);
     driver.findElement(By.xpath("//div[@id='x48761']/div[3]/div/div/div/div/div/div/div/div/div/div[6]/a/span")).click();
     driver.findElement(By.xpath("//input[@id='']")).click();
     driver.findElement(By.xpath("//input[@id='']")).clear();
     driver.findElement(By.xpath("//input[@id='']")).sendKeys("Тинькофф Мобайл");
+    Thread.sleep(3000);
     driver.findElement(By.xpath("//div[@id='search-and-pay-container']/div[2]/div[2]/div/form/div[3]/div/div/div/div[2]/div/div/div/div/div/div/div/div")).click();
+    Thread.sleep(1000);
     driver.findElement(By.xpath("//input[@id='']")).click();
+    Thread.sleep(1000);
     driver.findElement(By.xpath("//input[@id='']")).clear();
     driver.findElement(By.xpath("//input[@id='']")).sendKeys("5");
     driver.findElement(By.xpath("//h2")).click();
@@ -22,7 +26,6 @@ public class PaymentTest extends BaseRunner {
     assertEquals(driver.findElement(By.xpath("//div[2]/div/div/div/div/form/div/div/div[2]")).getText(), "Поле обязательное");
     driver.findElement(By.xpath("//form/div[2]/div/div/div/div/div/div/div/div[2]")).click();
     assertEquals(driver.findElement(By.xpath("//form/div[2]/div/div/div/div/div/div/div/div[2]")).getText(), "Минимум — 10 ₽");
-
   }
 }
 
