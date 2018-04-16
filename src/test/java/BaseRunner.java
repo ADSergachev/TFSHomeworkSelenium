768b528bc8a3df80a0474a0d21788219ec5fd0f0
ADSergachev <sergachevad@gmail.com>
2018-04-16 03:07:34 +0300

    main homework done

--- src/test/java/BaseRunner.java
+++ src/test/java/BaseRunner.java
@@ -1,6 +1,5 @@
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -23,6 +22,8 @@ public class BaseRunner {
     }
     driver.manage().window().maximize();
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+    driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
 
     Runtime.getRuntime().addShutdownHook(new Thread(() -> {
       driver.quit();
--- src/test/java/PaymentTest.java
+++ src/test/java/PaymentTest.java
@@ -1,107 +1,63 @@
 import static org.testng.AssertJUnit.assertEquals;
 
 import java.util.ArrayList;
+import java.util.List;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 
 public class PaymentTest extends BaseRunner {
   @Test
-  public void testByXpath() throws InterruptedException {
-    driver.get(BASE_URL);
-    driver.findElement(By.xpath("//*[@id=\"firstMenu\"]/li[5]/span/a/span")).click();
-    driver.findElement(By.xpath("//*[@id=\"xca7ce\"]/div[3]/div/div/div/div[1]/div/div/div/div/div/div[2]/a/span")).click();
-
-    ((JavascriptExecutor)driver).executeScript("window.scrollBy(" + 0 + "," + 20 + ");");
-    WebElement el = driver.findElement(By.xpath("//*[@id=\"x1f5c2\"]/div/div/div/div[2]/span/span"));
-    String result = el.getText();
-    result = result.substring(0 , result.length() - 2);
-    final int checkResult = Integer.parseInt(result);
-
-    Thread.sleep(1500);
-    driver.findElement(By.xpath("//*[@id=\"x1f5c2\"]/div/div/div/div[1]/div[1]/div/div[2]/div[2]/div/div[2]/ul/li[1]")).click();
-    driver.findElement(By.xpath("//*[@id=\"x1f5c2\"]/div/div/div/div[1]/div[1]/div/div[1]/div[2]/div/div[2]/ul/li[1]")).click();
-    driver.findElement(By.xpath("//*[@id=\"x1f5c2\"]/div/div/div/div[1]/div[1]/span/div[2]/div[1]/div/div/div[2]/span/span")).click();
-    driver.findElement(By.xpath("//*[@id=\"x1f5c2\"]/div/div/div/div[1]/div[1]/span/div[2]/div[2]/div/div/div[2]/span/span")).click();
-    Thread.sleep(3500);
-
-    el = driver.findElement(By.xpath("//*[@id=\"x1f5c2\"]/div/div/div/div[2]/span/span"));
-    result = el.getText();
-    result = result.substring(0 , result.length() - 2);
-    int resultCalc = Integer.parseInt(result);
-    if (resultCalc == 0) {
-      System.out.println("Calc OK");
-    } else {
-      throw new NullPointerException("Calculator is wrong");
-    }
-
-    if (!driver.findElement(By.xpath("//*[@id=\"form-application\"]//button")).isEnabled()){
-      System.out.println("button OK");
-    } else {
-        throw new NullPointerException("Button is wrong");
-    }
-    driver.navigate().refresh();
-
-    ((JavascriptExecutor)driver).executeScript("window.scrollBy(" + 0 + "," + 20 + ");");
-    Thread.sleep(4500);
-    el = driver.findElement(By.xpath("//*[@id=\"x1f5c2\"]/div/div/div/div[2]/span/span"));
-    result = el.getText();
-    result = result.substring(0 , result.length() - 2);
-    resultCalc = Integer.parseInt(result);
-    if (resultCalc == checkResult) {
-      System.out.println("Calk OK");
-    } else {
-      throw new NullPointerException("Calculator is wrong");
-    }
-    Thread.sleep(1500);
-    driver.findElement(By.xpath("//*[@id=\"x1f5c2\"]/div/div/div/div[1]/div[1]/div/div[2]/div[2]/div/div[2]/ul/li[6]")).click();
-    Thread.sleep(1500);
-    Boolean isPresentOtherService = driver.findElements(By.xpath("//*[@id=\"x1f5c2\"]/div/div/div/div[1]/div[1]/span")).size() > 0;
-    if (!isPresentOtherService){
-      System.out.println("OtherService OK");
-    } else {
-      throw new NoSuchElementException("OtherService is wrong");
-    }
-
-    el = driver.findElement(By.xpath("//*[@id=\"x1f5c2\"]/div/div/div/div[2]/span/span"));
-    result = el.getText();
-    result = result.substring(0 , result.length() - 2);
-    int unlimit = Integer.parseInt(result);
-
-    System.out.println(unlimit);
-  }
-
-  @Test
-  public void testPayment() throws InterruptedException {
+  public void testPaymentXPath() {
     driver.get(GOOGLE_URL);
     driver.findElement(By.xpath("//*[@id=\"lst-ib\"]")).click();
     driver.findElement(By.xpath("//*[@id=\"lst-ib\"]")).sendKeys("тинькофф");
     driver.findElement(By.xpath("//*[@id=\"lst-ib\"]")).sendKeys(Keys.ENTER);
     driver.findElement(By.xpath("//*[text() = 'https://www.tinkoff.ru/']/../../../..//*[@class=\"r\"]//a")).click();
     ArrayList<String> newTab = new ArrayList<>(driver.getWindowHandles());
-    driver.switchTo().window(newTab.get(1));
+    driver.switchTo().window(newTab.get(newTab.size() - 1));
     driver.findElement(By.xpath("//*[@id=\"x48761\"]/div[3]/div/div/div/div/div/div/div[6]/a")).click();
-    driver.findElement(By.xpath("//input[@id='']")).click();
-    driver.findElement(By.xpath("//input[@id='']")).clear();
-    driver.findElement(By.xpath("//input[@id='']")).sendKeys("Тинькофф Мобайл");
-    Thread.sleep(3000);
-    driver.findElement(By
-        .xpath("//div[@id='search-and-pay-container']/div[2]/div[2]/div/form/div[3]/div/div/div/div[2]/div/div/div/div/div/div/div/div"))
-        .click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//input[@id='']")).click();
-    Thread.sleep(1000);
-    driver.findElement(By.xpath("//input[@id='']")).clear();
-    driver.findElement(By.xpath("//input[@id='']")).sendKeys("5");
-    driver.findElement(By.xpath("//h2")).click();
-    driver.findElement(By.xpath("//div[2]/div/div/div/div/form/div/div/div[2]")).click();
+    driver.findElement(By.xpath("//*[@id=\"search-and-pay-container\"]/div[2]/div[2]/div/form/div[3]/div/div/div/div/label/div/input")).click();
+    driver.findElement(By.xpath("//*[@id=\"search-and-pay-container\"]/div[2]/div[2]/div/form/div[3]/div/div/div/div/label/div/input")).clear();
+    driver.findElement(By.xpath("//*[@id=\"search-and-pay-container\"]/div[2]/div[2]/div/form/div[3]/div/div/div/div/label/div/input")).sendKeys("Тинькофф Мобайл");
+    driver.findElement(By.xpath("//*[@id=\"search-and-pay-container\"]/div[2]/div[2]/div/form/div[3]/div/div/div/div[2]/div/div[1]/div/div/div[1]/div")).click();
+    driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[1]/div[5]/div/div[2]/div/div/div/div[1]/form/div[2]/div/div/div/div/div/div/div/div/label/div/input")).click();
+    driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[1]/div[5]/div/div[2]/div/div/div/div[1]/form/div[2]/div/div/div/div/div/div/div/div/label/div/input")).clear();
+    driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[1]/div[5]/div/div[2]/div/div/div/div[1]/form/div[2]/div/div/div/div/div/div/div/div/label/div/input")).sendKeys("5");
+    driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[1]/div[5]/div/div[2]/div/div/div/div[1]/form/div[4]/div/div[1]/div/div/button")).click();
     assertEquals(driver.findElement(By.xpath("//div[2]/div/div/div/div/form/div/div/div[2]")).getText(), "Поле обязательное");
-    driver.findElement(By.xpath("//form/div[2]/div/div/div/div/div/div/div/div[2]")).click();
     assertEquals(driver.findElement(By.xpath("//form/div[2]/div/div/div/div/div/div/div/div[2]")).getText(), "Минимум — 10 ₽");
+  }
+
+  @Test
+  public void testPaymentCSSSelectors(){
+    driver.get(GOOGLE_URL);
+    driver.findElement(By.cssSelector("#lst-ib")).click();
+    driver.findElement(By.cssSelector("#lst-ib")).sendKeys("тинькофф");
+    driver.findElement(By.cssSelector("#lst-ib")).sendKeys(Keys.ENTER);
+    List <WebElement> links = driver.findElements(By.cssSelector(".f > cite"));
+    List <WebElement> cites = driver.findElements(By.cssSelector(".rc > h3 > a"));
+    for (int i = 0; i < links.size(); i++){
+      WebElement a = cites.get(i);
+      WebElement b = links.get(i);
+      if (b.getText().equals("https://www.tinkoff.ru/")){
+        a.click();
+      }
+    }
+    ArrayList<String> newTab = new ArrayList<>(driver.getWindowHandles());
+    driver.switchTo().window(newTab.get(newTab.size() - 1));
+    driver.findElement(By.cssSelector("#x48761 > div.SecondMenu__root_3k8f6.SecondMenu__root_border_3F0bm.SecondMenu__root_hideOnPhone_3wL3A > div > div > div > div > div > div > div:nth-child(6) > a")).click();
+    driver.findElement(By.cssSelector("#search-and-pay-container > div.Banner__root_3tcdX > div.Container__container_1WsuA.Container__container_authorized_false_2dESI.Container__containerNoPhonePadding_q6z3q > div > form > div.ui-form__row.SearchAndPayHeader__form__field_1mwtG.SearchAndPayHeader__form__field_to_3tf_N > div > div > div > div > label > div > input")).click();
+    driver.findElement(By.cssSelector("#search-and-pay-container > div.Banner__root_3tcdX > div.Container__container_1WsuA.Container__container_authorized_false_2dESI.Container__containerNoPhonePadding_q6z3q > div > form > div.ui-form__row.SearchAndPayHeader__form__field_1mwtG.SearchAndPayHeader__form__field_to_3tf_N > div > div > div > div > label > div > input")).clear();
+    driver.findElement(By.cssSelector("#search-and-pay-container > div.Banner__root_3tcdX > div.Container__container_1WsuA.Container__container_authorized_false_2dESI.Container__containerNoPhonePadding_q6z3q > div > form > div.ui-form__row.SearchAndPayHeader__form__field_1mwtG.SearchAndPayHeader__form__field_to_3tf_N > div > div > div > div > label > div > input")).sendKeys("Тинькофф Мобайл");
+    driver.findElement(By.cssSelector("#search-and-pay-container > div.Banner__root_3tcdX > div.Container__container_1WsuA.Container__container_authorized_false_2dESI.Container__containerNoPhonePadding_q6z3q > div > form > div.ui-form__row.SearchAndPayHeader__form__field_1mwtG.SearchAndPayHeader__form__field_to_3tf_N > div > div > div > div.SearchSuggested__suggestContainer_Z6mjO > div > div:nth-child(1) > div > div > div:nth-child(1) > div")).click();
+    driver.findElement(By.cssSelector("body > div.application > div > div > div.PlatformLayout__layoutPage_WoIKN > div.PortalContainer__container_lyBzt > div.UILayoutPage__page_19-Kp > div:nth-child(1) > div.PlatformLayout__layoutPageComponent_3W4dc > div > div:nth-child(2) > div > div > div > div.Grid__column_3qcJA.Grid__column_size_8_13Rlm > form > div:nth-child(2) > div > div > div > div > div > div > div > div > label > div > input")).click();
+    driver.findElement(By.cssSelector("body > div.application > div > div > div.PlatformLayout__layoutPage_WoIKN > div.PortalContainer__container_lyBzt > div.UILayoutPage__page_19-Kp > div:nth-child(1) > div.PlatformLayout__layoutPageComponent_3W4dc > div > div:nth-child(2) > div > div > div > div.Grid__column_3qcJA.Grid__column_size_8_13Rlm > form > div:nth-child(2) > div > div > div > div > div > div > div > div > label > div > input")).clear();
+    driver.findElement(By.cssSelector("body > div.application > div > div > div.PlatformLayout__layoutPage_WoIKN > div.PortalContainer__container_lyBzt > div.UILayoutPage__page_19-Kp > div:nth-child(1) > div.PlatformLayout__layoutPageComponent_3W4dc > div > div:nth-child(2) > div > div > div > div.Grid__column_3qcJA.Grid__column_size_8_13Rlm > form > div:nth-child(2) > div > div > div > div > div > div > div > div > label > div > input")).sendKeys("5");
+    driver.findElement(By.cssSelector("body > div.application > div > div > div.PlatformLayout__layoutPage_WoIKN > div.PortalContainer__container_lyBzt > div.UILayoutPage__page_19-Kp > div:nth-child(1) > div.PlatformLayout__layoutPageComponent_3W4dc > div > div:nth-child(2) > div > div > div > div.Grid__column_3qcJA.Grid__column_size_8_13Rlm > form > div:nth-child(4) > div > div.ui-form__row.ui-form__row_component.ui-form__row_button-submit > div > div > button")).click();
+    assertEquals(driver.findElement(By.cssSelector("body > div.application > div > div > div.PlatformLayout__layoutPage_WoIKN > div.PortalContainer__container_lyBzt > div.UILayoutPage__page_19-Kp > div:nth-child(1) > div.PlatformLayout__layoutPageComponent_3W4dc > div > div:nth-child(2) > div > div > div > div.Grid__column_3qcJA.Grid__column_size_8_13Rlm > form > div.ui-form__row.ui-form__row_phone.ui-form__row_default-error-view-visible > div > div.ui-form-field-error-message.ui-form-field-error-message_ui-form")).getText(), "Поле обязательное");
+    assertEquals(driver.findElement(By.cssSelector("body > div.application > div > div > div.PlatformLayout__layoutPage_WoIKN > div.PortalContainer__container_lyBzt > div.UILayoutPage__page_19-Kp > div:nth-child(1) > div.PlatformLayout__layoutPageComponent_3W4dc > div > div:nth-child(2) > div > div > div > div.Grid__column_3qcJA.Grid__column_size_8_13Rlm > form > div.ui-form__row.ui-form__row_combination > div > div > div > div > div > div > div > div.ui-form-field-error-message.ui-form-field-error-message_ui-form")).getText(), "Минимум — 10 ₽");
   }
 }
