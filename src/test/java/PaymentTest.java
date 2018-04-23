4ec01f02f4f9b4204659da5861b24951aa686853
ADSergachev <sergachevad@gmail.com>
2018-04-23 16:23:46 +0300

    pageObject

--- src/test/java/PaymentTest.java
+++ src/test/java/PaymentTest.java
@@ -8,7 +8,48 @@ public class PaymentTest extends BaseRunner {
     tinkoffSearcher.goToTinkoff();
     tinkoffMainPage.goToPayments();
     tinkoffPayments.goToTinkoffMobile("Тинькофф Мобайл");
+    //Проверка ошибок
+    providerTinkoffMobile.clickPayButton();
+    //Телефон не заполнен
+    providerTinkoffMobile.numberValidator("Поле обязательное");
 
+    //Сумма не заполнена
+    providerTinkoffMobile.summValidator("Поле обязательное");
+
+    //Номер заполнен неверно
+    providerTinkoffMobile.setNumberOfTelephone("000");
+    providerTinkoffMobile.clickPayButton();
+    providerTinkoffMobile.numberValidator("Поле неправильно заполнено");
+
+    //Сумма меньше 10
+    providerTinkoffMobile.setSumm("0");
+    providerTinkoffMobile.clickPayButton();
+    providerTinkoffMobile.summValidator("Минимум — 10 \u20BD");
+
+    //сумма больше 15000
+    providerTinkoffMobile.setSumm("42340");
+    providerTinkoffMobile.clickPayButton();
+    providerTinkoffMobile.summValidator("Максимум — 15 000 \u20BD");
+
+    //неверное заполнение суммы
+    providerTinkoffMobile.setSumm("45aaaa");
+    providerTinkoffMobile.clickPayButton();
+    providerTinkoffMobile.sumFieldTextValidator("45");
+
+    //максимально допустимое количество символов ввода
+    providerTinkoffMobile.setNumberOfTelephone("00000000000000000");
+    providerTinkoffMobile.clickPayButton();
+    providerTinkoffMobile.numberFieldValidator("+7 (000) 000-00-00");
+
+    //запрет ввода любых символов кроме цифр
+    providerTinkoffMobile.setNumberOfTelephone("aaaaaaa");
+    providerTinkoffMobile.clickPayButton();
+    providerTinkoffMobile.numberFieldValidator("+7 (");
+
+    //максимально допустимое количество символов ввода
+    providerTinkoffMobile.maximizeSumm();
+    providerTinkoffMobile.clickPayButton();
+    providerTinkoffMobile.sumFieldTextValidator("0");
   }
 
 }
--- src/test/java/pages/ProviderTinkoffMobile.java
+++ src/test/java/pages/ProviderTinkoffMobile.java
@@ -2,6 +2,7 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class ProviderTinkoffMobile extends Page {
 
@@ -9,19 +10,49 @@ public class ProviderTinkoffMobile extends Page {
     super(driver);
   }
 
-  By numberOfTelephone = By.xpath("//*[@type = 'tel' and @name = 'provider-phone']");
-  By summ = By.xpath("//*[@class = 'Input__value_2Kx90']");
+  By numberOfTelephone = By.xpath("//*[@id=\"phone\"]");
+  By summ = By.xpath("/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[1]/div[5]/div/div[2]/div/div/div/div[1]/form/div[2]/div/div/div/div/div/div/div/div[1]/label/div/input");
+  By button = By.xpath("//*[@class='ui-button ui-button_failure ui-button_mobile-wide ui-button_provider-pay ui-button_size_xxl ui-form__button ui-button_inline']");
+  By numberError = By.xpath("/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[1]/div[5]/div/div[2]/div/div/div/div[1]/form/div[1]/div/div[2]");
+  By summError = By.xpath("/html/body/div[1]/div/div/div[2]/div[1]/div[2]/div[1]/div[5]/div/div[2]/div/div/div/div[1]/form/div[2]/div/div/div/div/div/div/div/div[2]");
 
-  public void setNumberOfTelephone(){
+  public void setNumberOfTelephone(String str){
     driver.findElement(numberOfTelephone).click();
     driver.findElement(numberOfTelephone).clear();
-    driver.findElement(numberOfTelephone).sendKeys("45454454");
+    driver.findElement(numberOfTelephone).sendKeys(str);
   }
 
-  public void setSumm(){
+  public void setSumm(String str){
     driver.findElement(summ).click();
     driver.findElement(summ).clear();
-    driver.findElement(summ).sendKeys("45");
+    driver.findElement(summ).sendKeys(str);
   }
 
+  public void clickPayButton(){
+    driver.findElement(button).click();
+  }
+
+  public void numberValidator(String error){
+    assertEquals(driver.findElement(numberError).getText(), error);
+  }
+
+  public void summValidator(String error){
+    assertEquals(driver.findElement(summError).getText(), error);
+  }
+
+  public void sumFieldTextValidator(String out){
+    assertEquals(driver.findElement(summ).getAttribute("value"), out);
+  }
+
+  public void numberFieldValidator(String out){
+    assertEquals(driver.findElement(numberOfTelephone).getAttribute("value"), out);
+  }
+
+  public void maximizeSumm(){
+    driver.findElement(summ).click();
+    driver.findElement(summ).clear();
+    for (int i = 0; i < 23; i++){
+      driver.findElement(summ).sendKeys("0");
+    }
+  }
 }
